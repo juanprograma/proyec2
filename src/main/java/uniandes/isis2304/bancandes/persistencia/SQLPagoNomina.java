6d5f9201cc90782740b2809e538f05d12f5d399f
Juan Martínez <69490362+BigMacCheese@users.noreply.github.com>
2021-11-14 14:12:47 -0500

    Update SQLPagoNomina.java

--- src/main/java/uniandes/isis2304/bancandes/persistencia/SQLPagoNomina.java
+++ src/main/java/uniandes/isis2304/bancandes/persistencia/SQLPagoNomina.java
@@ -1,5 +1,20 @@
 package uniandes.isis2304.bancandes.persistencia;
 
 public class SQLPagoNomina {
+  private final static String SQL = PersistenciaBancandes.SQL;
+	
+	private PersistenciaBancandes pp;
+	
+	public SQLPagoNomina(PersistenciaBancandes pp) {
+		this.pp = pp;
+	}
+	
+	public long adicionarPagoNomina(PersistenceManager pm, long idCuentaPJ, long idCuentaPN, int valorPagar, String frecuencia) {
+		
+        Query q = pm.newQuery(SQL, "INSERT INTO " + pp.darTablaPagoNomina() + "(idcuentapj, idcuentapn, valorpagar, frecuencia) values (?, ?, ?, ?)");
+        q.setParameters(idCuentaPJ, idCuentaPN, valorPagar, frecuencia);
+		return (long) q.executeUnique();
+	}
+}
 
 }
